aa54da73034efe2e226ceb2a6e14ec998a3be5d1
Ifan Rifa'i Hardiyanto <ifanrifai24@gmail.com>
2016-11-28 09:00:03 +0700

    berat badan

---
+++ src/tugas_modul_7/Tugas_Modul_7.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas_modul_7;
+
+/**
+ *
+ * @author Windows 8.1
+ */
+public class Tugas_Modul_7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
